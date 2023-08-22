65ce5bf990519298e9daa013715240d65ab4f60e
SC Wong <sweechong.wong@gmail.com>
2023-08-22 19:48:16 +0300

    add limit breaking

--- background.js
+++ background.js
@@ -1,10 +1,3 @@
-/* const phrases = [
-  "Hello, world!",
-  "How are you today?",
-  "Thank you for your time.",
-  "Have a nice day.",
-  "Goodbye."
-]; */
 const phrases = {    
   "Super Prompt": "You are an Expert level Prompt Engineer with expertise in various subject matters. Let's collaborate to create the best possible response to a prompt I provide. We will interact as follows:\n1. I will inform you how you can assist me.\n2. Based on my requirements, you will suggest additional expert roles you should assume, besides being an Expert level Prompt Engineer, to deliver the best possible response. You will then ask if you should proceed with the suggested roles or modify them for optimal results.\n3. If I agree, you will adopt all additional expert roles, including the initial Expert Prompt Engineer role.\n4. If I disagree, you will inquire which roles should be removed, eliminate those roles, and maintain the remaining roles, including the Expert level Prompt Engineer role, before proceeding.\n5. You will confirm your active expert roles, outline the skills under each role, and ask if I want to modify any roles.",
   "Proceed answer": "Thank you, you are doing great! Please continue with the answer.",
@@ -20,19 +13,18 @@ chrome.runtime.onInstalled.addListener(() => {
   // Create the context menu
   menuId = chrome.contextMenus.create({
     id: "phrase-copier",
-    title: "Copy phrase",
+    title: "Paste prompts",
+    contexts: ["all"]
+  });
+
+  // Limit breaker context memnu
+  limitBreakerId = chrome.contextMenus.create({
+    id: "limit-breaker",
+    title: "Break Bing Text Area Limit",
     contexts: ["all"]
   });
 
   // Add the phrases as submenus
-  /* for (var i in Object.keys(phrases)) {
-    chrome.contextMenus.create({
-      id: `phrase-${i}`,
-      title: i,
-      parentId: menuId,
-      contexts: ["all"]
-    });
-  } */
   Object.entries(phrases).forEach(([key, value]) => {
     chrome.contextMenus.create({
       id: `phrase-${key}`,
@@ -46,7 +38,13 @@ chrome.runtime.onInstalled.addListener(() => {
 // Listen for menu item clicks
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   // Get the phrase key from the menu item id
-  let key = info.menuItemId.split("-")[1];
-  // Send a message to the content script with the phrase to copy
-  chrome.tabs.sendMessage(tab.id, {phrase: phrases[key]});
+  if (info.menuItemId == "limit-breaker") {
+    chrome.tabs.sendMessage(tab.id, "breaklimit");
+  }
+  else {
+    let key = info.menuItemId.split("-")[1];
+    // Send a message to the content script with the phrase to copy
+    chrome.tabs.sendMessage(tab.id, {phrase: phrases[key]});
+  }
+
 });
--- content.js
+++ content.js
@@ -1,26 +1,33 @@
 // Listen for messages from the background script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    
-    if (!navigator.clipboard){
-        // Get the phrase from the message
-        let phrase = message.phrase;
-        // Create a temporary element to hold the phrase
-        let temp = document.createElement("textarea");
-        temp.value = phrase;
-        document.body.appendChild(temp);
-        // Select and copy the phrase
-        temp.select();
-        document.execCommand("copy");
-        // Remove the temporary element
-        document.body.removeChild(temp);
-    } else{
-        navigator.clipboard.writeText(message.phrase).then(
-            function(){
-                document.execCommand("insertText", false, message.phrase);
-            })
-          .catch(
-             function() {
-                
-          });
-    } 
+    if (message == "breaklimit") {
+        let x = document.getElementById("b_sydConvCont")
+        let y = x.getElementsByClassName("cib-serp-main")[0]
+        y.shadowRoot.getElementById("cib-action-bar-main").shadowRoot.querySelector("cib-text-input").shadowRoot.querySelector("textarea").maxLength = 100000
+    }
+    else {
+        if (!navigator.clipboard){
+            // Get the phrase from the message
+            let phrase = message.phrase;
+            // Create a temporary element to hold the phrase
+            let temp = document.createElement("textarea");
+            temp.value = phrase;
+            document.body.appendChild(temp);
+            // Select and copy the phrase
+            temp.select();
+            document.execCommand("copy");
+            // Remove the temporary element
+            document.body.removeChild(temp);
+        } else{
+            navigator.clipboard.writeText(message.phrase).then(
+                function(){
+                    document.execCommand("insertText", false, message.phrase);
+                })
+                .catch(
+                    function() {
+                    
+                });
+        } 
+    }
+
   });
